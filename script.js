a5ac13ac2d3122b7a707a0f71e54355fe383eba0
ABIN KUMAR M <abinkumarmuruganatham2006@gmail.com>
2026-06-25 01:45:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,51 @@ document.getElementById("themeBtn")
 document.body.classList.toggle("dark");
 
 });
+const counters =
+document.querySelectorAll(".counter");
+
+counters.forEach(counter=>{
+
+let target =
++counter.getAttribute("data-target");
+
+let count = 0;
+
+let update = ()=>{
+
+count += Math.ceil(target/60);
+
+if(count > target){
+count = target;
+}
+
+counter.innerText = count;
+
+if(count < target){
+requestAnimationFrame(update);
+}
+
+};
+
+update();
+
+});
+
+window.addEventListener("scroll",()=>{
+
+document
+.querySelectorAll(".reveal")
+.forEach(el=>{
+
+const top =
+el.getBoundingClientRect().top;
+
+if(top < window.innerHeight-100){
+
+el.classList.add("active");
+
+}
+
+});
+
+});
